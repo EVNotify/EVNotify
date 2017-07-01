8f9c147ea125568e5a90b89c7bd1aa088f7c2e99
GPlay97 <GPlay97@users.noreply.github.com>
2017-07-01 23:31:01 +0200

    implement translation handling; first german translation - very first verison of german translation file - translation handling including translate function; translate verification and translatePage handler function

---
+++ app/www/lng/de.js
@@ -0,0 +1,9 @@
+de = JSON.parse('{\
+    "TITLE_EVNOTIFY": "EVNotify",\
+    "HEADER_OVERVIEW": "Übersicht",\
+    "MENU_OVERVIEW": "Übersicht",\
+    "MENU_CHARGE": "Ladezyklen",\
+    "MENU_NOTIFYME": "NotifyMe",\
+    "MENU_SETTINGS": "Einstellungen"\
+    "MISSING_CONFIGURATION": "Kein Gerät und/oder Auto ausgewählt. Bitte in den Einstellungen entsprechend eintragen"\
+}');
