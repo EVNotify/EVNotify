e13a5d1b538ef6f587ef54edaf9e0c26ea84e9f8
GPlay97 <GPlay97@users.noreply.github.com>
2017-07-01 23:34:11 +0200

    add storage handling - implement set, get and remove value functions for localStorage

---
+++ app/www/js/storage.js
@@ -0,0 +1,20 @@
+function setValue(key, value) {
+    if(key && value && typeof key === 'string')
+        window.localStorage.setItem(key, ((typeof value === 'object')? JSON.stringify(value) : value));
+
+    return "";
+}
+
+function getValue(key, defaultValue) {
+    if(key && typeof key === 'string') {
+        var value = window.localStorage.getItem(key);
+        if(value) return value;
+        else if (defaultValue) return defaultValue;
+        else return "";
+    }
+}
+
+function removeValue(key) {
+    if(key && typeof key === 'string') window.localStorage.removeItem(key);
+    return "";
+}
