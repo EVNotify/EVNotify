a28efc8ac695958809b5e029df620f2da9bdaff8
GPlay97 <GPlay97@users.noreply.github.com>
2017-07-04 14:38:43 +0200

    add first version of english translation

---
+++ app/www/lng/en.js
@@ -0,0 +1,8 @@
+en = JSON.parse('{\
+    "HEADER_OVERVIEW": "Overview",\
+    "NEXT": "Next",\
+    "CANCEL": "Cancel",\
+    "LANGUAGE_SETUP": "Language selection",\
+    "LNG_EN": "English",\
+    "LNG_DE": "German"\
+}');
