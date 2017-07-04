694e0e13ab4b76333d82f9eb3cfd65731ea24551
GPlay97 <GPlay97@users.noreply.github.com>
2017-07-04 17:16:15 +0200

    complete english translation - add complete english translation

--- app/www/lng/en.js
+++ app/www/lng/en.js
@@ -1,8 +1,74 @@
 en = JSON.parse('{\
+    "TITLE_EVNOTIFY": "EVNotify",\
     "HEADER_OVERVIEW": "Overview",\
+    "MENU_OVERVIEW": "Overview",\
+    "MENU_CHARGE": "Charging cycles",\
+    "MENU_NOTIFYME": "NotifyMe",\
+    "MENU_SETTINGS": "Settings",\
+    "MISSING_CONFIGURATION": "No device or car selected. Please visit the settings.",\
     "NEXT": "Next",\
     "CANCEL": "Cancel",\
     "LANGUAGE_SETUP": "Language selection",\
     "LNG_EN": "English",\
-    "LNG_DE": "German"\
+    "LNG_DE": "German",\
+    "WELCOME": "Welcome to EVNotify",\
+    "WELCOME_TEXT": "Before you can be notified, you first have to create an account.\
+    It is required to manage your notifications. Ensure that you have an active internet and bluetooth connection.\
+    The app also requires to have a OBD2-Dongle. Please turn bluetooth on every time you use EVNotify.",\
+    "REGISTER": "Create account",\
+    "REGISTER_TEXT": "Almost there. Misses only a password. For your own safety please use a strong password (at least with 6 characters).",\
+    "PASSWORD_LENGTH_ERROR": "The password must be at least 6 characters.",\
+    "CONNECTION_ERROR": "Network error. Please try again.",\
+    "SETUP_COMPLETE": "Done!",\
+    "SETUP_COMPLETE_TEXT": "Du bist nun bereit, den Ladezustand Deines Elektroautos zu überwachen. Alles was Du nun tun musst, ist Deine gewünschten\
+    Benachrichtigunswege anzugeben und den OBD2-Dongle, welcher an die OBD2-Schnittstelle des Autos angesteckt werden muss, auszuwählen.\
+    Anschließend speichern. Alle Einstellungen können jederzeit angepasst werden. Bitte beachte, dass manche OBD2-Dongle auch dann aktiv bleiben, wenn das Auto längst aus ist.\
+    Lasse den OBD2-Dongle daher nicht über einen längeren Zeitraum angesteckt. Falls Du Fragen oder Fehler gefunden hast, geh bitte in das entsprechende Repository auf GitHub.\
+    EVNotify befindet sich noch im Entwicklungsstadium. Bitte nicht vergessen - es steckt viel Arbeit hinter all dem. Wenn was nicht klappt, melden - dann wird es behoben ;-)",\
+    "LOGIN": "Login",\
+    "LOGIN_TEXT": "Login with your existing account.",\
+    "LOGIN_FAILED": "Login failed.",\
+    "LOGIN_SUCCESSFUL": "Login successful.",\
+    "OLD_PASSWORD": "Old password",\
+    "OLD_PASSWORD_TEXT": "Please enter your old password.",\
+    "NEW_PASSWORD": "New password",\
+    "NEW_PASSWORD_TEXT": "Now enter your new password.",\
+    "NEW_PASSWORD_RETYPE": "Retype password",\
+    "NEW_PASSWORD_RETYPE_TEXT": "Retype your new password for confirmation.",\
+    "PASSWORD_MISMATCH": "The passwords do not match.",\
+    "CHANGE_PASSWORD_FAILED": "Password change failed.",\
+    "CHANGE_PASSWORD_SUCCESSFUL": "Password successfully changed.",\
+    "CHARGING_STATE": "State of charge ",\
+    "NOTIFICATION_SENT_ERROR": "An error occured trying to sent the notifications.",\
+    "BLUETOOTH_DATA_SENT_ERROR": "An error occured trying to send data via bluetooth.",\
+    "BLUETOOTH_CONNECTION_ERROR": "A bluetooth connection could not be established with the device.",\
+    "BLUETOOTH_DISABLED": "Bluetooth is disabled. Please turn bluetooth on!",\
+    "UNSUPPORTED_CAR": "The car is currently not supported. More cars will be added soon.",\
+    "UNSUPPORTED_DEVICE": "Your platform is not supported. A web-version is coming soon.",\
+    "SETTINGS_PASSWORD": "Password",\
+    "SETTINGS_PASSWORD_TEXT": "Please enter your password to save the settings.",\
+    "SET_SETTINGS_FAILED": "Saving the settings failed.",\
+    "SET_SETTINGS_SUCCESSFUL": "The settings have been saved successfully.",\
+    "HEADER_SETTINGS": "Settings",\
+    "SETTINGS_ACCOUNT": "Account",\
+    "SETTINGS_AKEY": "AKey",\
+    "CHANGE_PASSWORD": "Change password",\
+    "LANGUAGE": "Language",\
+    "NOTIFICATIONS": "Notifications",\
+    "EMAIL_NOTIFICATION": "Mail-Adress",\
+    "PUSH_NOTIFICATION": "Push-Notification",\
+    "TELEGRAM_NOTIFICATION": "Telegram-Notification",\
+    "TELEGRAM_MANAGE_NOTIFICATION": "Manage Telegram integration",\
+    "SETTINGS_SOC_HELP": "The desired state of charge at which the notifications should be sent.",\
+    "SETTINGS_SYSTEM": "System",\
+    "SETTINGS_DEVICES": "Bluetooth-Device",\
+    "SETTINGS_POLLING": "Polling-Interval",\
+    "POLLING_30": "every 30 seconds",\
+    "POLLING_60": "every minute",\
+    "POLLING_300": "every 5 minutes",\
+    "POLLING_600": "every 10 minutes",\
+    "POLLING_1800": "every half an hour",\
+    "POLLING_3600": "every hour",\
+    "POLLING_WARNING": "Short polling intervals lead to increased battery consumption.",\
+    "SAVE_SETTINGS": "Save settings"\
 }');
