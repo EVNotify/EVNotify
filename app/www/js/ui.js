01c3261e3e0cf6415907ca96ea1246dfc48d789f
GPlay97 <GPlay97@users.noreply.github.com>
2017-12-09 22:20:56 +0100

    first implementation of standby mode for obd2 dongle - also first version of dark mode possibility (enables after standby mode)

---
+++ app/www/js/ui.js
@@ -0,0 +1,27 @@
+/**
+ * @file        ui.js
+ * @description This contains several helper functions for the UI
+ */
+
+/**
+ * Enables or disables dark mode and handles styling for given page
+ * @param  {Boolean} enable whether or not dark mode shpuld be enabled
+ * @param  {String} page    the page (specific handling)
+ * @return {void}
+ */
+function darkMode(enable, page) {
+    // validate paramaters
+    if(typeof enable !== 'boolean' || typeof page !== 'string') return;
+
+    var pageContent = document.getElementById('pageContent');
+
+    if(pageContent) {
+        // switch handling for pages
+        if(page === 'index') {
+            pageContent.className = 'mdl-layout__content' + ((enable)? ' dark' : '');
+            document.getElementById('timestamp_icon').src = './icons/last_update' + ((enable)? '_light' : '') + '.svg';
+            document.getElementById('range_icon').src = './icons/ev_station' + ((enable)? '_light' : '') + '.svg';
+            document.getElementById('duration_icon').src = './icons/clock' + ((enable)? '_light' : '') + '.svg';
+        }
+    }
+}
