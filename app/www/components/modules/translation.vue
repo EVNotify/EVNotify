443f2f96fb4df0907a405a842d83d9df2bc6d60c
GPlay97 <GPlay97@users.noreply.github.com>
2018-07-17 21:50:22 +0200

    modify translation to use getLocalLng function

--- app/www/components/modules/translation.vue
+++ app/www/components/modules/translation.vue
@@ -6,17 +6,23 @@ import en from './../../lng/en.js';
 
 export default {
     /**
+     * Gets the current set local language or gets the default user preferred browser language
+     * @returns {String} the local lng (either 'de' or 'en')
+     */
+    getLocalLng: () => {
+        var lng = storage.getValue('lng', (navigator.language || navigator.userLanguage));
+
+        if(typeof lng === 'string' && lng.toLowerCase().indexOf('de') !== -1) return 'de';
+
+        return 'en';
+    },
+    /**
      * Translates given key into it's local translation (either from set language or user preferred language)
      * @returns {String} the translated string
      */
-    translate: (key) => {
-        var lng = storage.getValue('lng', (navigator.language || navigator.userLanguage));
-
-        if(typeof lng === 'string') {
-            if(lng.toUpperCase().indexOf('DE') !== -1) return de[key] || '';
-            return en[key] || '';
-        }
-        return '';
+    translate(key) {
+        if(this.getLocalLng() === 'de') return de[key] || '';
+        return en[key] || '';
     },
     /**
      * Translates each key from language and returns translated object
