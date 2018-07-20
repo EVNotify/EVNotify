a70f985e2275a5baaade67f1da741b04533e38f6
GPlay97 <GPlay97@users.noreply.github.com>
2018-07-20 23:41:31 +0200

    add selection handling for bottom bar container

--- app/www/components/container/bottom-bar.vue
+++ app/www/components/container/bottom-bar.vue
@@ -1,8 +1,22 @@
 <template>
-    <md-bottom-bar md-type="shift">
-        <md-bottom-bar-item md-label="Dashboard" md-icon="dashboard" to="/dashboard"></md-bottom-bar-item>
-        <md-bottom-bar-item md-label="Logs" md-icon="list" to="/logs"></md-bottom-bar-item>
-        <md-bottom-bar-item md-label="Stations" md-icon="ev_station" to="/stations"></md-bottom-bar-item>
-        <md-bottom-bar-item md-label="Settings" md-icon="settings" to="/settings"></md-bottom-bar-item>
+    <md-bottom-bar md-type="shift" :md-active-item.sync="currentSelection">
+        <md-bottom-bar-item id="bottom-bar-item-dashboard" md-label="Dashboard" md-icon="dashboard" to="/dashboard"></md-bottom-bar-item>
+        <md-bottom-bar-item id="bottom-bar-item-logs" md-label="Logs" md-icon="list" to="/logs"></md-bottom-bar-item>
+        <md-bottom-bar-item id="bottom-bar-item-stations" md-label="Stations" md-icon="ev_station" to="/stations"></md-bottom-bar-item>
+        <md-bottom-bar-item id="bottom-bar-item-settings" md-label="Settings" md-icon="settings" to="/settings"></md-bottom-bar-item>
     </md-bottom-bar>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentSelection: 'dashboard'
+        };
+    },
+    created() {
+        this.currentSelection = 'bottom-bar-item-' + this.$route.path.substr(1);
+    }
+}
+</script>
+
