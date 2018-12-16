da8d4f4d09dd7a3e5893795428b8bdf5c3ff5537
GPlay97 <GPlay97@users.noreply.github.com>
2018-12-16 12:28:08 +0100

    add filter for some cars to increase stability

--- app/www/components/cars/SOUL_EV.vue
+++ app/www/components/cars/SOUL_EV.vue
@@ -7,7 +7,7 @@
         data() {
             return {
                 initCMD: [
-                    'ATD', 'ATZ', 'ATE0', 'ATL0', 'ATS0', 'ATH1', 'AT0', 'ATSTFF', 'ATFE', 'ATSP6'
+                    'ATD', 'ATZ', 'ATE0', 'ATL0', 'ATS0', 'ATH1', 'AT0', 'ATSTFF', 'ATFE', 'ATSP6', 'ATCRA7EC'
                 ],
                 inStandbyMode: false,
                 offset: 0,
