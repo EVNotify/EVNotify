64d027f4e6d460b08d8d45cd7cc33ce2a2463b1d
GPlay97 <GPlay97@users.noreply.github.com>
2019-01-15 12:07:58 +0100

    Update app/www/components/container/bottom-bar.vue
    Co-Authored-By: EVSalomon <46657932+EVSalomon@users.noreply.github.com>

--- app/www/components/container/bottom-bar.vue
+++ app/www/components/container/bottom-bar.vue
@@ -1,7 +1,7 @@
 <template>
     <md-bottom-bar md-type="shift" :md-active-item.sync="currentSelection">
         <md-bottom-bar-item id="bottom-bar-item-dashboard" :md-label="translated.DASHBOARD" to="/dashboard">
-            <img src='icons/white/dashboard.svg' />
+            <img src="icons/white/dashboard.svg" />
             <span class="md-bottom-bar-label">{{ translated.DASHBOARD }}</span>
         </md-bottom-bar-item>
         <md-bottom-bar-item id="bottom-bar-item-logs" :md-label="translated.LOGS" to="/logs">
