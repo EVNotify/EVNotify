24826e1d6cffac63eadb5f43e06b9d5747ff5b6c
GPlay97 <GPlay97@users.noreply.github.com>
2021-01-06 21:37:45 +0100

    Add experimental ID.3

--- app/www/components/pages/dashboard-page.vue
+++ app/www/components/pages/dashboard-page.vue
@@ -173,6 +173,7 @@
         <KONAEV ref="KONA_EV"></KONAEV>
         <ZOEQ210 ref="ZOE_Q210"></ZOEQ210>
         <NIROEV ref="NIRO_EV"></NIROEV>
+        <ID3 ref="ID_3"></ID3>
         <snackbar ref="snackbar"></snackbar>
         <bottom-bar class="v-step-3"></bottom-bar>
     </div>
@@ -198,6 +199,7 @@
     import KONAEV from './../cars/KONA_EV.vue';
     import ZOEQ210 from './../cars/ZOE_Q210.vue';
     import NIROEV from './../cars/NIRO_EV.vue';
+    import ID3 from './../cars/ID_3.vue';
 
     export default {
         data() {
@@ -222,7 +224,7 @@
                 lastResponse: 0,
                 obd2ErrorCount: 0,
                 consumption: 0,
-                supportedCars: ['IONIQ_BEV', 'IONIQ_HEV', 'IONIQ_PHEV', 'IONIQ_FL_EV', 'SOUL_EV', 'E_SOUL', 'AMPERA_E', 'BOLT_EV', 'KONA_EV', 'ZOE_Q210', 'NIRO_EV'],
+                supportedCars: ['IONIQ_BEV', 'IONIQ_HEV', 'IONIQ_PHEV', 'IONIQ_FL_EV', 'SOUL_EV', 'E_SOUL', 'AMPERA_E', 'BOLT_EV', 'KONA_EV', 'ZOE_Q210', 'NIRO_EV', 'ID_3'],
                 initialized: false,
                 translated: {},
                 isWaitingForEnable: false,
