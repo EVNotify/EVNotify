84da0dfce444511ed1c27a85fc49c04ddc34b3df
GPlay97 <GPlay97@users.noreply.github.com>
2021-04-04 23:56:55 +0200

    send without delay

--- app/www/components/cars/ID_3.vue
+++ app/www/components/cars/ID_3.vue
@@ -55,9 +55,9 @@
                     if (self.offset + 1 === self.initCMD.length) {
                         // init of dongle finished, parse data and just send the OBD2 command
                         eventBus.$emit('obd2Data', self.parseData(data));
-                        setTimeout(() => {
+                        // setTimeout(() => {
                             bluetoothSerial.write(self.commands[self.currentCommand] + '\r');
-                        }, 500);
+                        // }, 500);
                     } else bluetoothSerial.write(self.initCMD[++self.offset] + '\r');
                 }, err => console.error(err));
 
